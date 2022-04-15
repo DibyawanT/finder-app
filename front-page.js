fa046ef855f928c01780aa6e9a90d3c9b8d8fddb
Dibyawan Trivedi <dibyawant@gmail.com>
2022-04-15 14:19:09 +0530

    css-change

--- front-page.js
+++ front-page.js
@@ -1,4 +1,5 @@
-var arrayT = ["bambhole"];
+var arrayT = ["Find your perfect project buddy..."];
+var arrayTwo = ["finder"];
 var speed = 100;
 var textPosition= 0;
 
@@ -10,6 +11,19 @@ function typewriter(){
         setTimeout(typewriter,speed);
     }
 }
+function typewriter2(){
+    document.getElementById("finder").innerHTML = 
+    arrayTwo[0].substring(0,textPosition) + "<span>\u25ae</span>";
 
+    if(textPosition++ != arrayTwo[0].length){
+        setTimeout(typewriter2,150);
+    }
+}
 
+function nextpage(){
+    document.location.href="main.html"
+}
+window.addEventListener('load',typewriter2);
 window.addEventListener('load',typewriter);
+
+//setTimeout(nextpage,6000);
